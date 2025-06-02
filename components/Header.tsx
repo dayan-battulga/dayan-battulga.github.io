4d2b7f9618402319a8fe07715cfce484fc5f9c47
Dayan Battulga <dayanbattulga8@gmail.com>
2025-06-02 18:49:40 -0500

    Changed padding and blur fade into navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import { BlurFade } from './magicui/blur-fade'
 
 const Header = () => {
   let headerClass = 'flex items-center w-full justify-between py-10'
@@ -13,37 +14,44 @@ const Header = () => {
 
   return (
     <header className={headerClass}>
-      <Link href="/" aria-label={siteMetadata.headerTitle}>
-        <div className="flex items-center justify-between">
-          {typeof siteMetadata.headerTitle === 'string' ? (
-            <>
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-              <div className="h-6 text-6xl font-semibold sm:hidden">DB</div>
-            </>
-          ) : (
-            siteMetadata.headerTitle
-          )}
+      <BlurFade
+        key="nav-bar"
+        delay={0.1}
+        inView
+        className="flex w-full items-center justify-between"
+      >
+        <Link href="/" aria-label={siteMetadata.headerTitle}>
+          <div className="flex items-center justify-between">
+            {typeof siteMetadata.headerTitle === 'string' ? (
+              <>
+                <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+                <div className="h-6 text-6xl font-semibold sm:hidden">DB</div>
+              </>
+            ) : (
+              siteMetadata.headerTitle
+            )}
+          </div>
+        </Link>
+        <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
+          <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
+            {headerNavLinks
+              .filter((link) => link.href !== '/')
+              .map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100"
+                >
+                  {link.title}
+                </Link>
+              ))}
+          </div>
+          <ThemeSwitch />
+          <MobileNav />
         </div>
-      </Link>
-      <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
-          {headerNavLinks
-            .filter((link) => link.href !== '/')
-            .map((link) => (
-              <Link
-                key={link.title}
-                href={link.href}
-                className="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100"
-              >
-                {link.title}
-              </Link>
-            ))}
-        </div>
-        <ThemeSwitch />
-        <MobileNav />
-      </div>
+      </BlurFade>
     </header>
   )
 }
